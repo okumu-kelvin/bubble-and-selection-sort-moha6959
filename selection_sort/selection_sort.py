f02c6e028bd1fc028b0ef247bd64739d0ef5181b
moha6959 <mohamedekaden576@gmail.com>
2025-06-20 12:05:13 +0300

    Selection sort exercise

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,18 @@
+def main():
+    my_array = [3,2,1]
+
+    # Ascending sort
+    results = selection_sort(my_array)
+    print( results)
 def selection_sort(arr):
     # TODO: Implement selection sort
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
     pass
+main()
