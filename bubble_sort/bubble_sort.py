715d40c4afdc795972bb78d4ce1ba4ce24ccc95e
moha6959 <mohamedekaden576@gmail.com>
2025-06-20 12:18:32 +0300

    bubble sort revised

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -3,28 +3,21 @@ def main():
     results = bubble_sort(my_array)
     print(results)
     
-    #results=bubble_reverse(my_array)
-    #print(results)
-# def bubble_duplicate():
-#    pass
-# def bubble_reverse(my_array):
-#     sorted_list=bubble_sort(my_array)
-#     return sorted_list[::]
-#     pass 
-def bubble_sort(my_array):
+    
+def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
     # my_array=[1,2,3]
-    n=len(my_array)
+    n=len(unsorted_list)
     for i in range(n-1):
         swapped=False
         for j in range(n-i-1):
-            if my_array[j]>my_array[j+1]:
-                my_array[j],my_array[j+1]=my_array[j+1],my_array[j]
+            if unsorted_list[j]>unsorted_list[j+1]:
+                unsorted_list[j],unsorted_list[j+1]=unsorted_list[j+1],unsorted_list[j]
                 swapped=True
         if not swapped:
                 break
-    print("Sorted array:", my_array)
-    return my_array 
+    print("Sorted array:", unsorted_list)
+    return unsorted_list 
     
 main()
 
